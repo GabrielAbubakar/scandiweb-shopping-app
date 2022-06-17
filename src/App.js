7ece06af073f18fddd382d753273a7018cf5f524
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-06-17 16:24:18 +0100

    miscellanous

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductLandingPage from './pages/ProductLandingPage'
 
 
 const App = () => {
     return (
-        <div>
-            <nav>
-                <ul className="links">
-                    <li>Women</li>
-                    <li>Men</li>
-                    <li>Kids</li>
-                </ul>
-            </nav>
-        </div >
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ProductLandingPage />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
