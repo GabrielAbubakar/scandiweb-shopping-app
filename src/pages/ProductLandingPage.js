1da07eaaa5c97b6b1a870f7ed25a0ee95cce78e8
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-06-17 21:32:15 +0100

    basic data fetch

--- src/pages/ProductLandingPage.js
+++ src/pages/ProductLandingPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { gql } from '@apollo/client';
+import client from '../graphql/apolloClient'
 
 
 class ProductLandingPage extends React.Component {
@@ -6,10 +8,37 @@ class ProductLandingPage extends React.Component {
         super(props)
 
         this.state = {
-
+            products: []
         }
 
         this.handleEvent = this.handleEvent.bind(this)
+    }
+
+    componentDidMount() {
+        const getProducts = gql`
+        query {
+            category {
+                products {
+                  name
+                  id
+                  description
+                  inStock
+                  prices{
+                    currency {
+                      label
+                      symbol
+                    }
+                    amount
+                  }
+                }
+            }
+        }
+        `
+
+        client.query({
+            query: getProducts
+        }).then(res => { this.setState({ products: res.data.category.products }) })
+            .catch(e => console.log(e));
     }
 
     handleEvent() {
@@ -17,9 +46,19 @@ class ProductLandingPage extends React.Component {
     }
 
     render() {
+        // const { name, id } = this.state.products
+
         return (
             <>
                 <h1>Hello App</h1>
+                {console.log(this.state.products)}
+                <ul>
+                    {
+                        this.state.products && this.state.products.map(item => (
+                            <li key={item.id}>{item.name}</li>
+                        ))
+                    }
+                </ul>
             </>
         )
     }
